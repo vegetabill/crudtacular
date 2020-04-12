a18296335395a9938890321e8b60b41ae3690bf6
Bill D <355438+vegetabill@users.noreply.github.com>
2020-04-12 16:34:43 -0700

    initial basics

---
+++ index.js
@@ -0,0 +1,10 @@
+const Model = require('./lib/model');
+const Controller = require('./lib/controller');
+const Server = require('./lib/server');
+
+module.exports = {
+  Model,
+  Controller,
+  Server,
+  VERSION: '0.0.1'
+};
